01cb9a776935850d7bdcac6a4c902891b4799f20
Fabio Vilela <fbvilela@gmail.com>
2012-02-27 22:31:54 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabrique::Application.config.secret_token = 'd049e0cc87f58baa35d41f1039b124a326d0c7d798a61e36b026f6a73058b4dcfd15422784b73e42e5c92466f9c58dec4c714019990d3753e73ba37091dc0fb9'
